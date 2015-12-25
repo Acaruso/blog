45d73636b625525e28c848fd1e698a84c02b8012
Acaruso <acarusoa@umich.edu>
2015-12-25 15:24:38 -0500

    updated main.py

--- python/controllers/main.py
+++ python/controllers/main.py
@@ -5,30 +5,78 @@ mysql = MySQL()
 
 main = Blueprint('main', __name__, template_folder='views')
 
+class Entry:
+	def __init__(self, id, date, content):
+		self.id      = id
+		self.date    = date
+		self.content = content
+	
 def get_entry(id):
-	cur = mysql.connection.cursor()
+	cur   = mysql.connection.cursor()
 	query = "SELECT * FROM blog.BlogEntry WHERE entryid = " + str(id)
 	cur.execute(query)
-	return cur.fetchall()
+	res   = cur.fetchall()
+	entry = Entry(id, res[0][1], res[0][2])
+	return entry
 
-def get_entries():
-	cur = mysql.connection.cursor()
-	query = "SELECT * FROM blog.BlogEntry"
+def get_all_entries():
+	cur     = mysql.connection.cursor()
+	query   = "SELECT * FROM blog.BlogEntry"
+	size    = cur.execute(query)
+	res     = cur.fetchall()
+	entries = []
+	for i in range (0, size):
+		entry = Entry(res[i][0], res[i][1], res[i][2])
+		entries.append(entry)
+	return entries
+
+def get_range_entries(begin, end):
+	cur     = mysql.connection.cursor()
+	query   = "SELECT * FROM blog.BlogEntry WHERE entryid >= " + str(begin)
+	query  += " AND entryid <= " + str(end)
+	size    = cur.execute(query)
+	res     = cur.fetchall()
+	entries = []
+	for i in range (0, size):
+		entry = Entry(res[i][0], res[i][1], res[i][2])
+		entries.append(entry)
+	return entries
+
+def get_num_entries():
+	cur   = mysql.connection.cursor()
+	query = "SELECT COUNT(*) FROM blog.BlogEntry"
 	cur.execute(query)
-	return cur.fetchall()
+	return cur.fetchall()[0][0]
 
 @main.route('/')
 def main_route():
+
+	page = request.args.get('page')
+	if page == None or page == '':
+		page = 0
+
+	num_entries = get_num_entries()
+
+	begin = (page * 5) + 1
+	end   = begin + 4
+
+	if end > num_entries:
+		end = num_entries
+
+	entries = get_range_entries(begin, end)
+
+	older = False
+	newer = False
+
+	if page > 0:
+		newer = True
+	if end <= num_entries and num_entries > 5:
+		older = True
+
 	options = {
-		"str": "hi",
-		"str2": '''hello<br/>
-		<iframe width="420" height="315"
-		src="https://www.youtube.com/embed/ybqDzhaKH4A?autoplay=1">
-		</iframe><br/>
-		asdf
-		'''
+		"entries": entries,
+		"older"  : older,
+		"newer"  : newer
 	}
-	print get_entries()
-		
 	return render_template("index.html", **options)
 
